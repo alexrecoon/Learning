5ae908ed1cc14e4e5303cf37b6fbcffef79a36b7
alexrecoon <52834857+alexrecoon@users.noreply.github.com>
2020-05-06 16:42:08 +0300

    Перенес класс в пакет, переписал класс

---
+++ src/main/java/ru/stqa/source/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package ru.stqa.source;
+
+public class MyFirstProgram {
+    public static void main(String[] args){
+        int a = 7;
+        String b = "seven";
+        System.out.println(a + b);
+    }
+}
+
